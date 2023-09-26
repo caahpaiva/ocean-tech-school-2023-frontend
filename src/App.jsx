785d2f18fc1930fc48d6436d202d653c34243899
Carolini Paiva <142840023+caahpaiva@users.noreply.github.com>
2023-09-26 00:07:22 +0000

    Conteudo HTML e CSS movido para componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
 import './App.css'
+import Card from './components/Card/Card'
+
+
 
 function App() {
     return (
     <>
-      <div className="card">
-        <h2>Rick Sanchez</h2>
-
-        <div className="tags">
-          <div className="tag"> Status: Vivo </div>
-          <div className="tag"> Especíe: Humana </div>
-          <div className="tag"> Origem: Terra C-137 </div>
-        
-        </div>
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
-        <p></p>
-       
-      </div>
+    <Card />
+      
 
     </>
   )
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,17 @@
+import "./Card.css"
+
+export default function Card() {
+    return <div className="card">
+    <h2>Rick Sanchez</h2>
+
+    <div className="tags">
+      <div className="tag"> Status: Vivo </div>
+      <div className="tag"> Especíe: Humana </div>
+      <div className="tag"> Origem: Terra C-137 </div>
+    
+    </div>
+    <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+    <p></p>
+   
+  </div>
+}
